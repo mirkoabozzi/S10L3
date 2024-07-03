8dab1568f353974b606bfe69decb1508e983081d
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-07-03 16:53:46 +0200

    commenti su movie details

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
-import { Col, Container, Image, Row, Spinner } from "react-bootstrap";
+import { Badge, Col, Container, Image, ListGroup, Row, Spinner } from "react-bootstrap";
 import { useNavigate, useParams } from "react-router-dom";
 
 const MovieDetails = () => {
   const [movie, setMovie] = useState(null);
+  const [comments, setComments] = useState([]);
   const params = useParams();
   const movieId = params.movieId;
-  const navigate = useNavigate();
+
+  //   console.log("comments", comments);
 
   const fetchMoviesDetails = async () => {
     try {
@@ -14,15 +16,34 @@ const MovieDetails = () => {
       if (response.ok) {
         setMovie(await response.json());
       } else {
-        throw new Error("Errore nel reperimento dei dati");
+        throw new Error("Errore nel reperimento del Film");
       }
     } catch (error) {
       console.console.log(error);
     }
   };
+  const fetchComments = async () => {
+    try {
+      const response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + movieId, {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZiZjk3OTdjMjM5YzAwMTUyZjRiM2IiLCJpYXQiOjE3MTk0ODcxNTcsImV4cCI6MTcyMDY5Njc1N30.cnWiTJ8Skk8km6KUyP5pIXFi42u-vAP3LJVqlx_JKd8",
+        },
+      });
+      if (response.ok) {
+        const result = await response.json();
+        setComments(result);
+      } else {
+        throw new Error("Errore nel recupero dei commenti");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     fetchMoviesDetails();
+    fetchComments();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [movieId]);
 
@@ -38,6 +59,27 @@ const MovieDetails = () => {
               <h1>{movie.Title}</h1>
               <p> Attori - {movie.Actors}</p>
               <p> Anno - {movie.Year}</p>
+            </Col>
+          </Row>
+          <Row className="mt-4">
+            <Col>
+              <ListGroup>
+                {comments.map((commento) => {
+                  return (
+                    <ListGroup.Item key={commento._id} title={commento.author}>
+                      <Row className="justify-content-between">
+                        <Col sm={8} md={6}>
+                          <h5>{commento.author}</h5>
+                          <p>{commento.comment}</p>
+                        </Col>
+                        <Col sm={3}>
+                          <Badge className="m-2">{commento.rate}</Badge>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  );
+                })}
+              </ListGroup>
             </Col>
           </Row>
         </>
--- src/components/TvShows.jsx
+++ src/components/TvShows.jsx
@@ -1,4 +1,4 @@
 const TvShows = () => {
-  return <div> TvShow </div>;
+  return <h1 className="text-center"> TvShow </h1>;
 };
 export default TvShows;
